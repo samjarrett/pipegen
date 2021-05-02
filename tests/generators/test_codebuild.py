652493feb2cf91b08b5f0bbd40bd95aefb3b482c
Sam Jarrett <samstah@gmail.com>
2021-05-02 16:33:31 +1000

    Add codebuild tests for get_codebuild_projects() and is_ecr()

--- tests/generators/test_codebuild.py
+++ tests/generators/test_codebuild.py
@@ -73,3 +73,30 @@ def test_generate_logical_id():
     assert codebuild.generate_logical_id("My App") == "CodeBuildMyApp"
     assert codebuild.generate_logical_id("My-App") == "CodeBuildMyApp"
     assert codebuild.generate_logical_id("My_App") == "CodeBuildMyApp"
+
+
+def test_get_codebuild_projects():
+    """Tests get_codebuild_projects()"""
+    config = {
+        "stages": [
+            {"enabled": True, "actions": ["action_one", "action_two"]},
+            {"enabled": False, "actions": ["action_three", "action_four"]},
+            {"enabled": True, "actions": ["action_five", "action_six"]},
+        ]
+    }
+    assert codebuild.get_codebuild_projects(config) == [
+        "action_one",
+        "action_two",
+        "action_five",
+        "action_six",
+    ]
+
+
+def test_is_ecr():
+    """Tests is_ecr()"""
+    assert codebuild.is_ecr("123456789012.dkr.ecr.us-east-1.amazonaws.com/repo") is True
+    assert (
+        codebuild.is_ecr("123456789012.dkr.ecr.us-east-1.amazonaws.com/repo:tag")
+        is True
+    )
+    assert codebuild.is_ecr("something_else") is False
